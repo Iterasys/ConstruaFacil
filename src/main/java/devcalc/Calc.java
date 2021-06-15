703c983c7ad7ec11130369f28d3d9a141115a7c7
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-06-14 22:32:04 -0300

    Criou o menu da Calculadora no metodo main, a chamada da função somarDoisNumeros e o seu respectivo teste unitario

---
+++ src/main/java/devcalc/Calc.java
@@ -0,0 +1,52 @@
+// 1 - Pacote
+package devcalc;
+// 2 - Bibliotecas
+import java.util.Scanner;
+
+// 3 - Classe
+public class Calc {
+    // 3.1 - Atributos
+    static Scanner entrada = new Scanner(System.in); // entrada = input
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args){
+
+        // Exibe o menu da calculadora
+        System.out.println(">>> CALCULADORA <<<");
+        System.out.println("(1) Somar");
+        System.out.println("(2) Subtrair");
+        System.out.println("(3) Multiplicar");
+        System.out.println("(4) Dividir");
+        System.out.print("Escolha o Calculo Desejado: ");
+
+        // Recebe a opção desejada
+        int opcao = entrada.nextInt();
+        int num1 = 0;
+        int num2 = 0;
+        // Pergunta os valores a serem usados no calculo
+
+        if (opcao >= 1 && opcao <= 4) {
+            System.out.print("Entre o 1º numero: ");
+            num1 = entrada.nextInt();
+            System.out.print("Entre o 2º numero: ");
+            num2 = entrada.nextInt();
+            System.out.print("O resultado é: ");
+        }
+
+        // Chama a função do calculo desejado
+        switch (opcao){
+            case 1:
+                System.out.println(somarDoisNumeros(num1, num2));
+                break;
+
+            default:
+                System.out.println("Opção invalida");
+                break;
+        }
+
+    }
+
+    public static int somarDoisNumeros(int num1, int num2){
+        return num1 + num2;
+    }
+}
---
+++ src/test/java/unitTests/TestaCalc.java
@@ -0,0 +1,28 @@
+package unitTests;
+
+import devcalc.Calc;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class TestaCalc {
+    @Test
+    public void testarSomarDoisNumeros() {
+        // 1 - Prepara - Configurar - Given
+        int num1 = 5;
+        int num2 = 7;
+        int resultadoEsperado = 12;
+
+        // 2 - Executar - When
+        int resultadoAtual = Calc.somarDoisNumeros(num1, num2);
+
+        // 3 - Validar - Then
+        System.out.println("O resultado Esperado é " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
+
+        assertEquals(resultadoEsperado,resultadoAtual);
+
+    }
+
+
+}
