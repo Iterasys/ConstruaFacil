f5499a2cb042f43956fe00069d3ef991f8ea4081
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-06-23 22:51:42 -0300

    Criação dos métodos PUT e DELETE para a entidade Pet

--- src/test/java/apiTests/Pet.java
+++ src/test/java/apiTests/Pet.java
@@ -13,7 +13,8 @@ import static org.hamcrest.Matchers.*;
 
 
 public class Pet {
-
+        String uri = "https://petstore.swagger.io/v2/pet";
+        int petId = 1101;
 
         // Padrão
         // Given = Dado
@@ -25,7 +26,7 @@ public class Pet {
                 return new String(Files.readAllBytes(Paths.get(caminhoJson)));
         }
 
-        @Test
+        @Test(priority = 0)
         public void incluirPet() throws IOException { // Create - Post
 
                 String jsonBody = lerJson("src/test/resources/data/pet.json");
@@ -37,28 +38,31 @@ public class Pet {
                         .log().all()                                    //Registrar tudo do envio
                         .body(jsonBody)
                 .when()                                                 //Quando
-                        .post("https://petstore.swagger.io/v2/pet")     //Comando + endpoint
+                        .post(uri)     //Comando + endpoint
                 .then()                                                 // Então
                         .log().all()                                    // Registrar tudo da volta
                         .statusCode(200)                                // Valida o Código do Estado Nativo
                         // .body("code", is(200))               // Valida o Código de Estado no Json
-                        .body("id", is(1101))        // Valida o id do animal
+                        .body("id", is(petId))        // Valida o id do animal
                         .body("name", is("Atena"))            // Valida o nome do animal
                         .body("category.name", is("dog"))       // Valida a categoria do animal
                         //.body("tags.name", not(contains("não vermifugado")))  // Valida se contem a palavra chava
-                        .body("tags.name", contains("vermifugado ok"))
+                        .body("tags.id[0]", is(3))
+                        .body("tags.name[0]", stringContainsInOrder("primeira semana"))
+                        .body("tags.id[1]", is(4))
+                        .body("tags.name[1]", stringContainsInOrder("segunda semana"))
                 ;
         }
 
-        @Test
+        @Test(priority = 1, dependsOnMethods = {"incluirPet"})
         public void consultarPet(){
-                String petId = "1101"; // Id do animal
+                //String petId = "1101"; // Id do animal
 
                 given()
                         .contentType("application/json")
                         .log().all()
                 .when()
-                        .get("https://petstore.swagger.io/v2/pet/" + petId) // Get == Consultar
+                        .get(uri + "/" + petId) // Get == Consultar
                 .then()
                    .log().all()
                    .statusCode(200)
@@ -66,5 +70,42 @@ public class Pet {
                    .body("status", is("available"))
                 ;
         }
+
+        @Test(priority = 2, dependsOnMethods = {"consultarPet"})
+        public void alterarPet() throws IOException {
+                String jsonBody = lerJson("src/test/resources/data/newpet.json");
+
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                        .body(jsonBody) // Json a ser transmitido para a alteração
+                .when()
+                        .put(uri)
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("status", is("sold"))
+                ;
+        }
+
+        //@Test(priority = 3, dependsOnMethods = {"alterarPet"})
+        @Test(priority = 3)
+        public void excluirPet(){
+
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                .when()
+                        .delete(uri + "/" + petId)
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("code", is(200))
+                        .body("type", is("unknown"))
+                        .body("message", is(Integer.toString(petId)))
+                ;
+        }
+
+
 
 }
