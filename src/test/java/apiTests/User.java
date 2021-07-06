febc9ab11aef4bd41d26d4e97691abaebb47e514
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-07-05 22:56:02 -0300

    Criação de script de teste web simples com Selenium WebDriver

--- src/test/java/apiTests/User.java
+++ src/test/java/apiTests/User.java
@@ -1,4 +1,100 @@
 package apiTests;
 
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+
 public class User {
+    String uri = "https://petstore.swagger.io/v2/user";
+    int userId = 2101;
+    String username = "donaldduck";
+    String password = "12345678";
+    String token = "";
+
+    // Padrão
+    // Given = Dado
+    // .When = Quando
+    // .Then = Então
+
+    // Funções de Apoio
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+    @Test
+    public void incluirUsuario() throws IOException {
+        String jsonBody = lerJson("src/test/resources/data/user.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .post(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(Integer.toString(userId)))
+        ;
+    }
+
+    @Test
+    public void consultarUsuario(){
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + username)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(userId))
+                .body("username", is(username))
+                .body("firstName", is("donald"))
+                .body("lastName", is("duck"))
+                .body("email", is("donaldduck@teste.com.br"))
+                .body("password", is("12345678"))
+                .body("phone", is("5511999999999"))
+                .body("userStatus", is(0))
+        ;
+
+    }
+    @Test
+    public void login(){
+
+        String mensagem =
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/login?username=" + username + "&password=" + password)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+        .extract()
+                .path("message")
+        ;
+
+        System.out.println("A mensagem é :" + mensagem);
+        token = mensagem.substring(23);
+        System.out.println("O token é :" + token);
+
+    }
+
+
+
+
+
+
 }
---
+++ src/test/java/webTests/seleniumSimples.java
@@ -0,0 +1,59 @@
+// 1 - Pacote
+package webTests;
+
+// 2 - Bibliotecas
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+
+// 3 - Classe
+public class seleniumSimples {
+    // 3.1 - Atributos
+
+    WebDriver driver;                           // declarar o objeto do Selenium WebDriver
+
+    // 3.2 - Métodos e Funções
+
+    @Test
+    public void consultarCurso(){
+
+        // A - Início
+        // Aponta para onde está o driver do Chrome
+        System.setProperty("webdriver.chrome.driver", "drivers/chrome/91/chromedriver.exe");
+        // Instancia o objeto driver como um controlador do Chrome
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+
+        // B - Realizar o teste
+        driver.get("https://www.iterasys.com.br");                              // Abre o site alvo informado
+
+        driver.findElement(By.id("searchtext")).click();                        // Clica no campo de pesquisa
+        driver.findElement(By.id("searchtext")).clear();                        // Limpa o campo de pesquisa
+        driver.findElement(By.id("searchtext")).sendKeys("mantis");  // Escreve "mantis" no campo
+
+        driver.findElement(By.id("btn_form_search")).click();                   // Clique na lupa
+
+        assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"mantis\"");
+        //assertTrue(driver.findElement(By.cssSelector("h3")).getText().contains("mantis"));
+        //assertEquals(driver.findElement(By.cssSelector("h3:nth-child(1)")).getText(), "Cursos › \"mantis\"");
+
+        driver.findElement(By.cssSelector("span.comprar")).click();             // Clica no botão Matricule-se
+
+        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Mantis");
+        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 49,99");
+
+
+        driver.quit(); // Encerra o objeto do Selenium
+
+    }
+
+}
