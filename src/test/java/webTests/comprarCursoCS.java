a37125b75cbb745661690da4c5d2c388c2535d13
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-07-19 22:45:34 -0300

    Cucumber + Selenium

---
+++ src/test/java/webTests/comprarCursoCS.java
@@ -0,0 +1,77 @@
+package webTests;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.E;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class comprarCursoCS {
+    WebDriver driver;
+
+    @Before
+    public void iniciar(){
+        System.setProperty("webdriver.chrome.driver", "drivers/chrome/91/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+        driver.manage().window().maximize(); // Maximizar a janela
+
+        System.out.println("0 - Antes do Teste iniciar");
+    }
+
+    @After
+    public void finalizar(){
+        driver.quit();
+        System.out.println("Z - Depois do Teste finalizar");
+    }
+
+
+    @Dado("^que acesso o site da Iterasys$")
+    public void queAcessoOSiteDaIterasys() {
+        driver.get("https://www.iterasys.com.br");
+        System.out.println("1 - Acessou o site");
+    }
+
+    @Quando("^pesquiso por \"([^\"]*)\"$")
+    public void pesquisoPor(String curso) {
+        driver.findElement(By.id("searchtext")).click();                //Opcional
+        driver.findElement(By.id("searchtext")).clear();                //Opcional
+        driver.findElement(By.id("searchtext")).sendKeys(Keys.chord(curso)); // Escreve o nome do curso letra por letra
+        System.out.println("2 - Digitou o nome do curso como " + curso);
+    }
+
+    @E("^clico na Lupa$")
+    public void clicoNaLupa() {
+        driver.findElement(By.id("btn_form_search")).click();
+        System.out.println("3 - Clicou na lupa");
+    }
+
+    @Entao("^vejo a lista de resultados para o curso \"([^\"]*)\"$")
+    public void vejoAListaDeResultadosParaOCurso(String curso)  {
+        assertEquals(driver.findElement(By.cssSelector("h3")).getText(),"Cursos › \"" + curso + "\"");
+        System.out.println("4 - Exibiu a lista de resultados para o curso " + curso);
+    }
+
+    @Quando("^clico em Matricule-se$")
+    public void clicoEmMatriculeSe() {
+        driver.findElement(By.cssSelector("span.comprar")).click();
+        System.out.println("5 - Clicou em Matricule-se");
+    }
+
+    @Entao("^confirmo o nome do curso como \"([^\"]*)\" e o preco de \"([^\"]*)\"$")
+    public void confirmoONomeDoCursoComoEOPrecoDe(String curso, String preco)  {
+        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), curso);
+        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), preco);
+        System.out.println("6 - Confirmou o nome como " + curso + " e o preco do curso como " + preco);
+
+    }
+}
---
+++ src/test/resources/features/comprarCursoCS.feature
@@ -0,0 +1,9 @@
+#language: pt
+Funcionalidade: Comprar Curso CS
+  Cenario: Pesquisar Curso e Incluir no Carrinho
+    Dado que acesso o site da Iterasys
+    Quando pesquiso por "Mantis"
+    E clico na Lupa
+    Entao vejo a lista de resultados para o curso "Mantis"
+    Quando clico em Matricule-se
+    Entao confirmo o nome do curso como "Mantis" e o preco de "R$ 49,99"
