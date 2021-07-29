df2e37492f37955abdfb44e3bcf55d1ff02ac2d0
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-07-28 22:35:49 -0300

    Utilização de espera explicita atraves da Classe WebDriverWait

--- src/test/java/webTests/comprarCursoCS.java
+++ src/test/java/webTests/comprarCursoCS.java
@@ -9,7 +9,10 @@ import cucumber.api.java.pt.Quando;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,19 +20,22 @@ import static org.testng.Assert.assertEquals;
 
 public class comprarCursoCS {
     WebDriver driver;
+    WebDriverWait wait;
 
     @Before
-    public void iniciar(){
+    public void iniciar() {
         System.setProperty("webdriver.chrome.driver", "drivers/chrome/91/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+        //driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
         driver.manage().window().maximize(); // Maximizar a janela
+
+        wait = new WebDriverWait(driver,5,1); // espera até 60 segundos
 
         System.out.println("0 - Antes do Teste iniciar");
     }
 
     @After
-    public void finalizar(){
+    public void finalizar() {
         driver.quit();
         System.out.println("Z - Depois do Teste finalizar");
     }
@@ -56,8 +62,15 @@ public class comprarCursoCS {
     }
 
     @Entao("^vejo a lista de resultados para o curso \"([^\"]*)\"$")
-    public void vejoAListaDeResultadosParaOCurso(String curso)  {
-        assertEquals(driver.findElement(By.cssSelector("h3")).getText(),"Cursos › \"" + curso + "\"");
+    public void vejoAListaDeResultadosParaOCurso(String curso) {
+        String textoEsperado = "Cursos › \"" + curso + "\"";
+
+        //wait.until(ExpectedConditions.textToBe(By.cssSelector("h3:nth-child(1)"),textoEsperado));
+        //assertEquals(driver.findElement(By.cssSelector("h3")).getText(),textoEsperado);
+
+        // Esperar que o elemento tenha o texto desejado
+        wait.until(ExpectedConditions.textToBe(By.cssSelector("h3"), textoEsperado));
+        assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"" + curso + "\"");
         System.out.println("4 - Exibiu a lista de resultados para o curso " + curso);
     }
 
@@ -68,10 +81,35 @@ public class comprarCursoCS {
     }
 
     @Entao("^confirmo o nome do curso como \"([^\"]*)\" e o preco de \"([^\"]*)\"$")
-    public void confirmoONomeDoCursoComoEOPrecoDe(String curso, String preco)  {
+    public void confirmoONomeDoCursoComoEOPrecoDe(String curso, String preco) {
+        //wait.until(ExpectedConditions.textToBe(By.cssSelector("span.item-title"),curso));
+
+        wait.until(ExpectedConditions.textToBe(By.cssSelector("span.item-title"),curso));
         assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), curso);
         assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), preco);
         System.out.println("6 - Confirmou o nome como " + curso + " e o preco do curso como " + preco);
 
+    }
+
+    @E("^pressiono Enter$")
+    public void pressionoEnter() {
+        driver.findElement(By.id("searchtext")).sendKeys(Keys.ENTER);
+        System.out.println("3a - Pressionou o Enter");
+    }
+
+    @Quando("^clico na imagem de um curso$")
+    public void clicoNaImagemDeUmCurso() {
+        driver.findElement(By.cssSelector("span.mais")).click();
+    }
+
+    @Entao("^vejo a pagina com detalhes do curso$")
+    public void vejoAPaginaComDetalhesDoCurso() {
+        wait.until(ExpectedConditions.titleIs("Mantis - Iterasys"));
+        assertEquals(driver.getTitle(),"Mantis - Iterasys");
+    }
+
+    @E("^clico no botao Ok do popup da LGPD$")
+    public void clicoNoBotaoOkDoPopupDaLGPD() {
+        driver.findElement(By.cssSelector("a.cc-btn.cc-dismiss")).click();
     }
 }
--- src/test/resources/features/comprarCursoCS.feature
+++ src/test/resources/features/comprarCursoCS.feature
@@ -1,9 +1,35 @@
 #language: pt
 Funcionalidade: Comprar Curso CS
-  Cenario: Pesquisar Curso e Incluir no Carrinho
+  Cenario: Pesquisar Curso com Clique e Incluir no Carrinho
     Dado que acesso o site da Iterasys
     Quando pesquiso por "Mantis"
     E clico na Lupa
     Entao vejo a lista de resultados para o curso "Mantis"
     Quando clico em Matricule-se
     Entao confirmo o nome do curso como "Mantis" e o preco de "R$ 49,99"
+
+  Cenario: Pesquisar Curso com Enter e Incluir no Carrinho
+    Dado que acesso o site da Iterasys
+    Quando pesquiso por "Mantis"
+    E pressiono Enter
+    Entao vejo a lista de resultados para o curso "Mantis"
+    Quando clico em Matricule-se
+    Entao confirmo o nome do curso como "Mantis" e o preco de "R$ 49,99"
+
+  Esquema do Cenario: Pesquisar Curso com Enter e Incluir no Carrinho
+    Dado que acesso o site da Iterasys
+    Quando pesquiso por <curso>
+    E pressiono Enter
+    Entao vejo a lista de resultados para o curso <curso>
+    Quando clico em Matricule-se
+    Entao confirmo o nome do curso como <curso> e o preco de <preco>
+    Exemplos:
+      | curso               |  preco      |
+      | "Mantis"            | "R$ 49,99"  |
+      | "Preparatório CTFL" | "R$ 169,00" |
+
+    Cenario: Ver Detalhes de Um Curso
+      Dado que acesso o site da Iterasys
+      E clico no botao Ok do popup da LGPD
+      Quando clico na imagem de um curso
+      Entao vejo a pagina com detalhes do curso
